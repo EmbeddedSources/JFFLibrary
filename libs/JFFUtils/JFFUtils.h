ab5402468db2fcb472a67e70aad0ded2f6771d63
volodg@mail.ru <volodg@mail.ru>
2011-05-29 03:50:39 +0300

    initial commit

--- libs/JFFUtils/JFFUtils.h
+++ libs/JFFUtils/JFFUtils.h
@@ -2,3 +2,7 @@
 #import <JFFUtils/NSObject/NSObject+OnDeallocBlock.h>
 
 #import <JFFUtils/NSArray/NSArray+BlocksAdditions.h>
+
+#import <JFFUtils/UIView/UIView+AnimationWithBlocks.h>
+
+#import <JFFUtils/UITableView/UITableView+WithinUpdates.h>
