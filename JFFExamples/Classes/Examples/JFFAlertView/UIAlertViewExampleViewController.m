6b5e52567f487fb1bf8f042150472ccc6465818f
volodg@mail.ru <volodg@mail.ru>
2011-05-31 23:26:03 +0300

    Add examples of using JFFLibraries

--- JFFExamples/Classes/Examples/JFFAlertView/UIAlertViewExampleViewController.m
+++ JFFExamples/Classes/Examples/JFFAlertView/UIAlertViewExampleViewController.m
@@ -2,11 +2,25 @@
 
 static NSString* const cancel_button_title_ = @"cancel";
 static NSString* const button1_button_title_ = @"button1";
+static NSString* const button2_button_title_ = @"button2";
+
+@interface UIAlertViewExampleViewController ()
+
+@property ( nonatomic, retain ) UIAlertView* alertView1;
+@property ( nonatomic, retain ) UIAlertView* alertView2;
+
+@end
 
 @implementation UIAlertViewExampleViewController
 
+@synthesize alertView1 = _alert_view1;
+@synthesize alertView2 = _alert_view2;
+
 -(void)dealloc
 {
+   [ _alert_view1 release ];
+   [ _alert_view2 release ];
+
    [ super dealloc ];
 }
 
@@ -27,57 +41,80 @@ static NSString* const button1_button_title_ = @"button1";
    return [ [ [ self alloc ] init ] autorelease ];
 }
 
-/*
-// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
-- (void)viewDidLoad {
-    [super viewDidLoad];
-}
-*/
-
-/*
-// Override to allow orientations other than the default portrait orientation.
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
-    // Return YES for supported orientations.
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
-}
-*/
-
--(void)viewDidUnload
+-(BOOL)shouldAutorotateToInterfaceOrientation:( UIInterfaceOrientation )interface_orientation_
 {
-   [ super viewDidUnload ];
+   return YES;
 }
 
-//UIAlertViewDelegate
+-(void)showAlertView1WithButton2:( BOOL )show_button2_
+{
+   self.alertView1 = [ [ [ UIAlertView alloc ] initWithTitle: @"Alert1"
+                                                     message: @"test"
+                                                    delegate: self
+                                           cancelButtonTitle: cancel_button_title_
+                                           otherButtonTitles: nil ] autorelease ];
+
+   if ( show_button2_ )
+   {
+      [ self.alertView1 addButtonWithTitle: button2_button_title_ ];
+   }
+   [ self.alertView1 addButtonWithTitle: button1_button_title_ ];
+
+   [ self.alertView1 show ];
+}
+
 -(IBAction)showAlertView1Action:( id )sender_
 {
-   UIAlertView* alert_ = [ [ UIAlertView alloc ] initWithTitle: @"Alert1"
-                                                       message: @"test"
-                                                      delegate: self
-                                             cancelButtonTitle: cancel_button_title_
-                                             otherButtonTitles: button1_button_title_, nil ];
-   [ alert_ show ];
-   [ alert_ release ];
+   [ self showAlertView1WithButton2: NO ];
 }
 
 -(IBAction)showAlertView2Action:( id )sender_
 {
+   [ self showAlertView1WithButton2: YES ];
 }
 
 -(IBAction)showAlertView3Action:( id )sender_
 {
+   self.alertView2 = [ [ [ UIAlertView alloc ] initWithTitle: @"Alert2"
+                                                     message: @"test"
+                                                    delegate: self
+                                           cancelButtonTitle: cancel_button_title_
+                                           otherButtonTitles: button1_button_title_, nil ] autorelease ];
+
+   [ self.alertView2 show ];
 }
 
 #pragma mark UIAlertViewDelegate
 
 -(void)alertView:( UIAlertView* )alert_view_ clickedButtonAtIndex:( NSInteger )button_index_
 {
-   if ( alert_view_.cancelButtonIndex == button_index_ )
+   NSString* clicked_button_title_ = [ alert_view_ buttonTitleAtIndex: button_index_ ];
+
+   if ( self.alertView1 == alert_view_ )
    {
-      NSLog( @"Alert1 \"%@\" button selected", cancel_button_title_ );
+      if ( [ clicked_button_title_ isEqualToString: cancel_button_title_ ] )
+      {
+         NSLog( @"Alert1 \"%@\" button selected with index: %d", cancel_button_title_, button_index_ );
+      }
+      else if ( [ clicked_button_title_ isEqualToString: button1_button_title_ ] )
+      {
+         NSLog( @"Alert1 \"%@\" button selected with index: %d", button1_button_title_, button_index_ );
+      }
+      else if ( [ clicked_button_title_ isEqualToString: button2_button_title_ ] )
+      {
+         NSLog( @"Alert1 \"%@\" button selected with index: %d", button2_button_title_, button_index_ );
+      }
    }
-   else
+   else if ( self.alertView2 == alert_view_ )
    {
-      NSLog( @"Alert1 \"%@\" button selected", button1_button_title_ );
+      if ( [ clicked_button_title_ isEqualToString: cancel_button_title_ ] )
+      {
+         NSLog( @"Alert2 \"%@\" button selected with index: %d", cancel_button_title_, button_index_ );
+      }
+      else if ( [ clicked_button_title_ isEqualToString: button1_button_title_ ] )
+      {
+         NSLog( @"Alert2 \"%@\" button selected with index: %d", button1_button_title_, button_index_ );
+      }
    }
 }
 
