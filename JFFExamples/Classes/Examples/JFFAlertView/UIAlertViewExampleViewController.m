1073b4d554cd07c55fd8c12d10e2eba02c44a5ac
volodg@mail.ru <volodg@mail.ru>
2011-05-31 22:46:02 +0300

    Add examples of using JFFLibraries

--- JFFExamples/Classes/Examples/JFFAlertView/UIAlertViewExampleViewController.m
+++ JFFExamples/Classes/Examples/JFFAlertView/UIAlertViewExampleViewController.m
@@ -1,5 +1,8 @@
 #import "UIAlertViewExampleViewController.h"
 
+static NSString* const cancel_button_title_ = @"cancel";
+static NSString* const button1_button_title_ = @"button1";
+
 @implementation UIAlertViewExampleViewController
 
 -(void)dealloc
@@ -50,8 +53,8 @@
    UIAlertView* alert_ = [ [ UIAlertView alloc ] initWithTitle: @"Alert1"
                                                        message: @"test"
                                                       delegate: self
-                                             cancelButtonTitle: @"cancel"
-                                             otherButtonTitles: @"button1", nil ];
+                                             cancelButtonTitle: cancel_button_title_
+                                             otherButtonTitles: button1_button_title_, nil ];
    [ alert_ show ];
    [ alert_ release ];
 }
@@ -68,6 +71,14 @@
 
 -(void)alertView:( UIAlertView* )alert_view_ clickedButtonAtIndex:( NSInteger )button_index_
 {
+   if ( alert_view_.cancelButtonIndex == button_index_ )
+   {
+      NSLog( @"Alert1 \"%@\" button selected", cancel_button_title_ );
+   }
+   else
+   {
+      NSLog( @"Alert1 \"%@\" button selected", button1_button_title_ );
+   }
 }
 
 @end
