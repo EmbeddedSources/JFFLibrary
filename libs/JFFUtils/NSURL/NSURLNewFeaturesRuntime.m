a935b168ab6cbf8e549fb055f12f4dabaa343a91
Vladimir Gorbenko <volodg@mail.ru>
2011-06-17 17:00:46 +0300

    add - [ NSURL pathComponents ] method for ios ver. < 4.0

--- libs/JFFUtils/NSURL/NSURLNewFeaturesRuntime.m
+++ libs/JFFUtils/NSURL/NSURLNewFeaturesRuntime.m
@@ -16,19 +16,7 @@
 
 -(NSArray*)pathComponents
 {
-   NSArray* components_ = [ [ self path ] componentsSeparatedByString: @"/" ];
-   NSUInteger components_count_ = [ components_ count ];
-   if ( components_count_ == 1 )
-   {
-      return [ NSArray array ];
-   }
-
-   NSMutableArray* result_ = [ NSMutableArray arrayWithObject: @"/" ];
-
-   NSArray* except_first_components_ = [ components_ subarrayWithRange: NSMakeRange( 1, components_count_ - 1 ) ];
-   [ result_ addObjectsFromArray: except_first_components_ ];
-
-   return result_;
+   return [ [ self path ] pathComponents ];
 }
 
 +(void)load
