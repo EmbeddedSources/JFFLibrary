00149cc235aafa747348562f917c5cf822d7a460
volodg@mail.ru <volodg@mail.ru>
2011-06-24 21:39:19 +0300

    Merge remote-tracking branch 'origin/master'

--- libs/JFFUtils/NSObject/NSObject+RuntimeExtensions.h
+++
@@ -1,8 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@interface NSObject (RuntimeExtensions)
-
-+(void)addMethodIfNeedWithSelector:( SEL )selector_
-                           toClass:( Class )class_;
-
-@end
