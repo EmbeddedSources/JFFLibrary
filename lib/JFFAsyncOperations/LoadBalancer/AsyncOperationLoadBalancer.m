6939d982eaedc567acaa5f8262825554ddafec6c
volodg@mail.ru <volodg@mail.ru>
2011-06-27 02:14:55 +0300

    Load balancer implementation

--- lib/JFFAsyncOperations/LoadBalancer/AsyncOperationLoadBalancer.m
+++ lib/JFFAsyncOperations/LoadBalancer/AsyncOperationLoadBalancer.m
@@ -6,9 +6,10 @@
 
 JFFAsyncOperation balancedAsyncOperation( JFFAsyncOperation loader_ )
 {
-   return ^( JFFAsyncOperationProgressHandler progress_callback_
+   return [ [ ^( JFFAsyncOperationProgressHandler progress_callback_
             , JFFCancelAsyncOperationHandler cancel_callback_
             , JFFDidFinishAsyncOperationHandler done_callback_ )
    {
-   }
+      return [ [ ^( BOOL canceled_ ) {} copy ] autorelease ];
+   } copy ] autorelease ];
 }
