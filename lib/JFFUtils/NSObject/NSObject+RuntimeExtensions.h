5c4bd74483ad1bf4784131cc011dfedad3f385ea
volodg@mail.ru <volodg@mail.ru>
2011-06-27 22:59:07 +0300

    add hook method to NSObject

--- lib/JFFUtils/NSObject/NSObject+RuntimeExtensions.h
+++ lib/JFFUtils/NSObject/NSObject+RuntimeExtensions.h
@@ -5,4 +5,9 @@
 +(void)addMethodIfNeedWithSelector:( SEL )selector_
                            toClass:( Class )class_;
 
++(void)hookMethodForClass:( Class )class_
+             withSelector:( SEL )target_selector_
+  prototypeMethodSelector:( SEL )prototype_selector_
+       hookMethodSelector:( SEL )hook_selector_;
+
 @end
--- lib/JFFUtils/NSObject/NSObject+RuntimeExtensions.m
+++ lib/JFFUtils/NSObject/NSObject+RuntimeExtensions.m
@@ -19,4 +19,22 @@
    }
 }
 
++(void)hookMethodForClass:( Class )class_
+             withSelector:( SEL )target_selector_
+  prototypeMethodSelector:( SEL )prototype_selector_
+       hookMethodSelector:( SEL )hook_selector_
+{
+   Method target_method_ = class_getInstanceMethod( class_, target_selector_ );
+   Method prototype_method_ = class_getInstanceMethod( [ self class ], prototype_selector_ );
+   const char* type_encoding_ = method_getTypeEncoding( prototype_method_ );
+   BOOL method_added_ = class_addMethod( class_
+                                        , hook_selector_
+                                        , method_getImplementation( prototype_method_ )
+                                        , type_encoding_ );
+   NSAssert( method_added_, @"should be added" );
+   Method hook_method_ = class_getInstanceMethod( class_, hook_selector_ );
+
+   method_exchangeImplementations( target_method_, hook_method_ );
+}
+
 @end
