dfcec8cd15f37144ff0171d095a5861d7280e145
oleksandr dodatko <dodikk88.reg@gmail.com>
2011-09-12 12:47:22 +0300

    Added a tracing "description" method

--- lib/JFFNetwork/JFFURLResponse.m
+++ lib/JFFNetwork/JFFURLResponse.m
@@ -17,4 +17,21 @@
    return [ [ _all_header_fields objectForKey: @"Content-Length" ] longLongValue ];
 }
 
+#pragma mark -
+#pragma mark NSObject
+-(NSString*)description
+{
+   NSMutableString* result_ = [ NSMutableString stringWithFormat: @"<<< JFFUrlResponse. HttpStatusCode = %d \n", self.statusCode ] ;
+   [ result_ appendFormat: @"Result length = %lld \n", self.expectedContentLength ];
+   [ result_ appendString: @"Headers : \n" ];
+
+   [ self.allHeaderFields enumerateKeysAndObjectsUsingBlock: ^(id key_, id obj_, BOOL* stop_)
+                                                             {
+                                                                [ result_ appendFormat: @"\t%@ = %@ \n", key_, obj_ ];
+                                                             } 
+   ];
+
+   return result_;
+}
+
 @end
