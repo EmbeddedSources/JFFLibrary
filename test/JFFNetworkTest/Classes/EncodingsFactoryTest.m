fa01cbb054868027edcdd00b980a4860482464fb
oleksandr dodatko <dodikk88.reg@gmail.com>
2011-09-13 15:49:33 +0300

    Added tests

---
+++ test/JFFNetworkTest/Classes/EncodingsFactoryTest.m
@@ -0,0 +1,13 @@
+//
+//  EncodingsFactoryTest.m
+//  JFFNetworkTest
+//
+//  Created by Oleksandr Dodatko on 9/13/11.
+//  Copyright (c) 2011 EPAM systems. All rights reserved.
+//
+
+#import "EncodingsFactoryTest.h"
+
+@implementation EncodingsFactoryTest
+
+@end
---
+++ test/JFFNetworkTest/Classes/StubDecoderTest.m
@@ -0,0 +1,13 @@
+//
+//  StubDecoderTest.m
+//  JFFNetworkTest
+//
+//  Created by Oleksandr Dodatko on 9/13/11.
+//  Copyright (c) 2011 EPAM systems. All rights reserved.
+//
+
+#import "StubDecoderTest.h"
+
+@implementation StubDecoderTest
+
+@end
