54936fbc2ee7cdfae479c91bed5ad7f3e16d34e7
Vladimir Gorbenko <volodg@mail.ru>
2011-09-14 09:57:33 +0300

    some cosmetic changes

--- test/JFFNetworkTest/Classes/EncodingsFactoryTest.m
+++ test/JFFNetworkTest/Classes/EncodingsFactoryTest.m
@@ -14,8 +14,8 @@
       GHAssertNotNil( decoder_, @"NOT nil data Expected" );
       GHAssertTrue( [ decoder_ conformsToProtocol: @protocol(JNHttpDecoder) ], @"This class should conform protocol" );
       GHAssertTrue( [ decoder_ isMemberOfClass   : [ JNGzipDecoder class ]  ], @"This should be JNGzipDecoder class" );
-   }   
-   
+   }
+
    {
       decoder_ = [ JNHttpEncodingsFactory decoderForHeaderString: @"gzip" ];
       GHAssertNotNil( decoder_, @"NOT nil data Expected" );
@@ -37,19 +37,19 @@
 
    {
       decoder_ = [ JNHttpEncodingsFactory decoderForHeaderString: @"" ];
-      
+
       GHAssertNotNil( decoder_, @"NOT nil data Expected" );
       GHAssertTrue( [ decoder_ conformsToProtocol: @protocol(JNHttpDecoder) ], @"This class should conform protocol" );
       GHAssertTrue( [ decoder_ isMemberOfClass   : [ JNStubDecoder class ]  ], @"This should be JNStubDecoder class" );
    }
-   
+
    {
       decoder_ = [ JNHttpEncodingsFactory decoderForHeaderString: nil ];
       GHAssertNotNil( decoder_, @"NOT nil data Expected" );
       GHAssertTrue( [ decoder_ conformsToProtocol: @protocol(JNHttpDecoder) ], @"This class should conform protocol" );
       GHAssertTrue( [ decoder_ isMemberOfClass   : [ JNStubDecoder class ]  ], @"This should be JNStubDecoder class" );
    }
-   
+
    {
       decoder_ = [ JNHttpEncodingsFactory decoderForHeaderString: @"abrakadabra" ];
       GHAssertNotNil( decoder_, @"NOT nil data Expected" );
--- test/JFFNetworkTest/Classes/GzipErrorsLoggerTest.m
+++ test/JFFNetworkTest/Classes/GzipErrorsLoggerTest.m
@@ -2,14 +2,13 @@
 
 @end
 
-
 @implementation GzipErrorsLoggerTest
 
 -(void)testGzipLoggerDoesNotAcceptValuesOutOf_minus1_minus6
 {
    NSString* received_ = nil;
    NSString* expected_ = nil;
-   
+
    {
       expected_ = @"Z_UnknownError";
       received_ = [ JNGzipErrorsLogger zipErrorFromCode: 0 ];
@@ -20,7 +19,7 @@
    {
       expected_ = @"Z_UnknownError";
       received_ = [ JNGzipErrorsLogger zipErrorFromCode: -7 ];
-      
+
       GHAssertTrue( [ expected_ isEqualToString: received_ ], @"Wrong description" );
    }
 }
@@ -34,42 +33,42 @@
    {
       expected_ = @"Z_ERRNO";
       received_ = [ JNGzipErrorsLogger zipErrorFromCode: Z_ERRNO ];
-      
+
       GHAssertTrue( [ expected_ isEqualToString: received_ ], @"Wrong description" );
    }   
-   
+
    {
       expected_ = @"Z_STREAM_ERROR";
       received_ = [ JNGzipErrorsLogger zipErrorFromCode: Z_STREAM_ERROR ];
-      
+
       GHAssertTrue( [ expected_ isEqualToString: received_ ], @"Wrong description" );
    }  
-   
+
    {
       expected_ = @"Z_DATA_ERROR";
       received_ = [ JNGzipErrorsLogger zipErrorFromCode: Z_DATA_ERROR ];
-      
+
       GHAssertTrue( [ expected_ isEqualToString: received_ ], @"Wrong description" );
    }  
-   
+
    {
       expected_ = @"Z_MEM_ERROR";
       received_ = [ JNGzipErrorsLogger zipErrorFromCode: Z_MEM_ERROR ];
-      
+
       GHAssertTrue( [ expected_ isEqualToString: received_ ], @"Wrong description" );
    }  
-   
+
    {
       expected_ = @"Z_BUF_ERROR";
       received_ = [ JNGzipErrorsLogger zipErrorFromCode: Z_BUF_ERROR ];
       
       GHAssertTrue( [ expected_ isEqualToString: received_ ], @"Wrong description" );
    }  
-   
+
    {
       expected_ = @"Z_VERSION_ERROR";
       received_ = [ JNGzipErrorsLogger zipErrorFromCode: Z_VERSION_ERROR ];
-      
+
       GHAssertTrue( [ expected_ isEqualToString: received_ ], @"Wrong description" );
    }    
 }
