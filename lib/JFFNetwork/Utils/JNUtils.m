c1953979c1693cc32da97519017134f0b387b869
oleksandr dodatko <dodikk88.reg@gmail.com>
2011-10-19 11:30:14 +0300

    Added convenience methods

--- lib/JFFNetwork/Utils/JNUtils.m
+++ lib/JFFNetwork/Utils/JNUtils.m
@@ -15,4 +15,18 @@
    return [ self headersDictionadyWithContentType: @"application/x-www-form-urlencoded" ];
 }
 
+#pragma mark -
+#pragma mark UTF8 xml
++(NSString*)utf8XmlContentType
+{
+   return @"application/xml;charset=utf-8";
+}
+
++(NSDictionary*)headersDictionadyWithUtf8XmlContentType
+{
+   return [ self headersDictionadyWithContentType: [ self utf8XmlContentType ] ];
+}
+
+
+
 @end
