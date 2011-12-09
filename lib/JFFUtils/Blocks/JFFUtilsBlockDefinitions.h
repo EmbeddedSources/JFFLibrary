9f1333ce87dba71e58c64e326e98de9b98a83c82
oleksandr dodatko <dodikk88.reg@gmail.com>
2011-12-09 11:10:15 +0200

    Added a missing include

--- lib/JFFUtils/Blocks/JFFUtilsBlockDefinitions.h
+++ lib/JFFUtils/Blocks/JFFUtilsBlockDefinitions.h
@@ -1,6 +1,8 @@
 #ifndef JFF_UTILS_BLOCK_DEFINITIONS
 #define JFF_UTILS_BLOCK_DEFINITIONS
 
+#import <Foundation/Foundation.h>
+
 typedef void (^JFFSimpleBlock)( void );
 typedef BOOL (^PredicateBlock)( id object_ );//JTODO renamr to JPredicateBlock
 
