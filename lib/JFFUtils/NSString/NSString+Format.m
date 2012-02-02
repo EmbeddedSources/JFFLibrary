15cf4654a7a4271a8a89fa49817020ba313d3c17
Vladimir Gorbenko <volodg@mail.ru>
2012-02-02 14:07:17 +0200

    add NSString+Format.h to target

--- lib/JFFUtils/NSString/NSString+Format.m
+++ lib/JFFUtils/NSString/NSString+Format.m
@@ -4,32 +4,32 @@
 
 +(id)stringWithFormatCheckNill:( NSString* )format_, ... 
 {
-   if ( [ format_ length ] == 0 )
-      return nil;
-   
-   id eachObject = nil;
-   va_list argument_list_;
+    if ( [ format_ length ] == 0 )
+        return nil;
 
-   va_start(argument_list_, format_ );
-   eachObject = va_arg( argument_list_, id);
-   
-   while ( eachObject )
-   {
-      if ( ![ eachObject isKindOfClass:[ NSObject class ] ] )
-             return nil;
-             
-      if ( [ [ eachObject description ] length ] == 0 )
-         return nil;
-      
-      eachObject = va_arg( argument_list_, id);
-   }
-   va_start(argument_list_, format_ );
-   return [ [ [ NSString alloc] initWithFormat: format_ arguments: argument_list_ ] autorelease ];
+    id eachObject_ = nil;
+    va_list argument_list_;
+
+    va_start(argument_list_, format_ );
+    eachObject_ = va_arg( argument_list_, id);
+
+    while ( eachObject_ )
+    {
+        if ( ![ eachObject_ isKindOfClass: [ NSObject class ] ] )
+            return nil;
+
+        if ( [ [ eachObject_ description ] length ] == 0 )
+            return nil;
+
+        eachObject_ = va_arg( argument_list_, id);
+    }
+    va_start(argument_list_, format_ );
+    return [ [ NSString alloc] initWithFormat: format_ arguments: argument_list_ ];
 }
 
 -(BOOL)hasSymbols
 {
-   return ![ self isEqualToString: @"" ];
+    return ![ self isEqualToString: @"" ];
 }
 
 @end
