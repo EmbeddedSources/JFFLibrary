0e8cf24b4808263b2911ca05a89c68d946beb0df
Vladimir Gorbenko <volodg@mail.ru>
2012-02-02 14:58:28 +0200

    dodikk - patch

--- test/JFFUtilsTest/Classes/EmptyStringTest.m
+++ test/JFFUtilsTest/Classes/EmptyStringTest.m
@@ -7,34 +7,34 @@
 
 -(void)testNilStringIsEmpty
 {
-   NSString* str_ = nil;
-   GHAssertFalse( [ str_ hasSymbols ], @"Nil String[%@] should have no symbols", str_ );
+    NSString* str_ = nil;
+    GHAssertFalse( [ str_ hasSymbols ], @"Nil String[%@] should have no symbols", str_ );
 }
 
 -(void)testEmptyStringIsEmpty
 {
-   {
-      NSString* str_ = @"";
-      GHAssertFalse( [ str_ hasSymbols ], @"Nil String[%@] should have no symbols", str_ );
-   }
-   
-   {
-      NSMutableString* empty_ = [ NSMutableString stringWithString: @"" ];
-      NSString* str_ = [ NSString stringWithString: empty_ ];
-      GHAssertFalse( [ str_ hasSymbols ], @"Nil String[%@] should have no symbols", str_ );
-   }
-   
-   {
-      NSMutableString* empty_ = [ NSMutableString stringWithString: @"" ];
-      NSString* str_ = [ [ empty_ copy ] autorelease ];
-      GHAssertFalse( [ str_ hasSymbols ], @"Nil String[%@] should have no symbols", str_ );
-   }
+    {
+        NSString* str_ = @"";
+        GHAssertFalse( [ str_ hasSymbols ], @"Nil String[%@] should have no symbols", str_ );
+    }
+
+    {
+        NSMutableString* empty_ = [ NSMutableString stringWithString: @"" ];
+        NSString* str_ = [ NSString stringWithString: empty_ ];
+        GHAssertFalse( [ str_ hasSymbols ], @"Nil String[%@] should have no symbols", str_ );
+    }
+
+    {
+        NSMutableString* empty_ = [ NSMutableString stringWithString: @"" ];
+        NSString* str_ = [ [ empty_ copy ] autorelease ];
+        GHAssertFalse( [ str_ hasSymbols ], @"Nil String[%@] should have no symbols", str_ );
+    }
 }
 
 -(void)testStringWithCharactersIsNotEmpty
 {
-   NSString* str_ = @"abrakadabre";
-   GHAssertTrue( [ str_ hasSymbols ], @"The String[%@] should have some symbols", str_ );
+    NSString* str_ = @"abrakadabre";
+    GHAssertTrue( [ str_ hasSymbols ], @"The String[%@] should have some symbols", str_ );
 }
 
 @end
--- test/JFFUtilsTest/Classes/JFFStringFormatTest.m
+++ test/JFFUtilsTest/Classes/JFFStringFormatTest.m
@@ -5,24 +5,23 @@
 
 -(void)testNotAllParamsPresent
 {
-   NSString* format_ = @"%@%@%@";
-   NSString* param1_ = @"1";
-   NSString* param2_ = @"2";
-   
-   NSString* result_ = [ NSString stringWithFormatCheckNill: format_, param1_, param2_, nil ];
-   GHAssertTrue( result_ != nil, @"Should NOT be nil value" );
+    NSString* format_ = @"%@%@%@";
+    NSString* param1_ = @"1";
+    NSString* param2_ = @"2";
+
+    NSString* result_ = [ NSString stringWithFormatCheckNill: format_, param1_, param2_, nil ];
+    GHAssertTrue( result_ != nil, @"Should NOT be nil value" );
 }
 
 -(void)testOneMoreParam
 {
-   NSString* format_ = @"%@%@";
-   NSString* param1_ = @"1";
-   NSString* param2_ = @"2";
-   NSString* param3_ = @"3";
-   
-   NSString* result_ = [ NSString stringWithFormatCheckNill: format_, param1_, param2_, param3_, nil ];
-   GHAssertTrue( result_ != nil, @"Should NOT be nil value" );
-}
+    NSString* format_ = @"%@%@";
+    NSString* param1_ = @"1";
+    NSString* param2_ = @"2";
+    NSString* param3_ = @"3";
 
+    NSString* result_ = [ NSString stringWithFormatCheckNill: format_, param1_, param2_, param3_, nil ];
+    GHAssertTrue( result_ != nil, @"Should NOT be nil value" );
+}
 
 @end
